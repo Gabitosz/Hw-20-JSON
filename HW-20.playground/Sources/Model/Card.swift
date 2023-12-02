ff243899a14657507c978600acd6f7da3a6ca8e3
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-12-02 21:22:54 +0200

    add Card model

---
+++ HW-20.playground/Sources/Model/Card.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+public struct Cards: Decodable {
+    public let cards: [Card]
+    
+    public init(cards: [Card]) {
+        self.cards = cards
+    }
+}
+
+public struct Card: Decodable {
+    public let name: String
+    public let type: String
+    public let manaCost: String?
+    public let set: String
+    public let rarity: String
+    public let power: String?
+    public let toughness: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case type
+        case manaCost
+        case set
+        case rarity
+        case power
+        case toughness
+    }
+    
+    public init(name: String, type: String, manaCost: String?, set: String, rarity: String, power: String?, toughness: String?) {
+        self.name = name
+        self.type = type
+        self.manaCost = manaCost
+        self.set = set
+        self.rarity = rarity
+        self.power = power
+        self.toughness = toughness
+    }
+}
+
